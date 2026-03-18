210673c766cd7e5a637785ff9aa6a5df928b8b9f
Rakesh <rakeshkumar102208@gmail.com>
2026-03-18 19:29:09 +0530

    count character from string without space count

---
+++ Count_CHAR.py
@@ -0,0 +1,10 @@
+name=input("Enter name:")
+char=input("Enter any charcter:")
+
+length=len(name.replace(" ",""))
+
+print("length (Excluding space):",length)
+
+count=name.lower().count(char.lower())
+
+print("Count of Charecters :",count)
