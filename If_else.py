20675d2beb808d4395e6b0c3060aab1abc3cddf6
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-03-28 11:54:26 +0530

    if_else practics

---
+++ If_else.py
@@ -0,0 +1,16 @@
+n=3
+
+if n%2== 0 :
+    if n>2 and n < 5:
+        print("Not Weird")
+        
+    elif n > 6 and n<20:
+        print("Weird")
+    elif n > 20:
+        print("Not weird")
+    else:
+        print("Not weird")
+        
+        
+else:
+    print("Weird")
